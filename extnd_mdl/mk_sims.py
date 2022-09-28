87a7a6c102e98f3855abf1e990817b77da09a39c
anahita.pakiman <anahita.pakiman@scai.fraunhofer.de>
2022-09-28 09:07:10 +0200

    add thickness step to parameter

--- extnd_mdl/mk_sims.py
+++ extnd_mdl/mk_sims.py
@@ -9,9 +9,10 @@ s = pd.DataFrame()
 
 
 ti = 1
-dt = 0.2
+dt = 0.05
 dt_ui = 0.6
-id = '1'
+id = '5'
+simName_f = 'CCSA_submodel_'
 
 # set 1 dt_tu= 0.6
 # set 2 dt=tu=0.4
@@ -20,32 +21,33 @@ id = '1'
 j, sims_id = int(id) * 1000, []
 tL = []
 for i in range(0, 6, 1):
-    t = 1 + i*0.2
+    t = 1 + i*dt
     tL.append(t)
     sims_id.append(j)
     j += 1
 tR = copy.copy(tL)
 
+
 tL0, tR0 = copy.copy(tL), copy.copy(tR)
 for i in range(1, 6, 1):
-    dt = i*0.2
+    dti = i*dt
 
     for ts in tL0:
         sims_id.append(j)
         # left stiff
-        tL.append(ts + dt)
+        tL.append(ts + dti)
         tR.append(ts)
 
         sims_id.append(j+1)
         # right stiff
         tL.append(ts)
-        tR.append(ts + dt)
+        tR.append(ts + dti)
         j += 2
 
-s['tL_u'] = tL
-s['tR_u'] = tR
-s['tL_i'] = [x + dt_ui for x in tL]
-s['tR_i'] = [x + dt_ui for x in tR]
+s['tL_i'] = tL
+s['tR_i'] = tR
+s['tL_u'] = [x + dt_ui for x in tL]
+s['tR_u'] = [x + dt_ui for x in tR]
 
 s['mL_u'] = ['2000121' for x in tL]
 s['mR_u'] = ['2000121' for x in tL]
@@ -53,6 +55,8 @@ s['mL_i'] = ['2000142' for x in tL]
 s['mR_i'] = ['2000142' for x in tL]
 s['dt_ui'] = str(dt_ui)
 s['id'] = sims_id
+
+s['name'] = [simName_f + str(ids) for ids in sims_id]
 
 s.to_pickle("./sims_extnd_0{}.pkl".format(id))
 input(s)
@@ -65,14 +69,15 @@ for jId, r in s.iterrows():
     print(r.tL_u, r.tR_u, r.tL_i, r.tR_i,
           r.mL_u, r.mR_u, r.mL_i, r.mR_i
           )
+
+    ids = '{}{:03d}'.format(id, jId)
+    simName = simName_f + ids
+
     keyUpdate = kInp.format(
         r.tL_u, r.tR_u, r.tL_i, r.tR_i,
-        r.mL_u, r.mR_u, r.mL_i, r.mR_i
+        r.mL_u, r.mR_u, r.mL_i, r.mR_i,
+        simName
     )
-
-    simName_f = 'CCSA_submodel_'
-    id = '{}{:03d}'.format(id, jId)
-    simName = simName_f + id
 
     studPath = os.path.join(data_path, simName)
     if not os.path.exists(studPath):
@@ -80,7 +85,8 @@ for jId, r in s.iterrows():
 
     outKey = simName + '.key'
     outKeyP = os.path.join(studPath, outKey)
+    print(outKeyP)
 
-    # with open(outKeyP, "w") as f:
-    #     f.write(keyUpdate)
-    #     print(outKeyP)
+    with open(outKeyP, "w") as f:
+        f.write(keyUpdate)
+        print(outKeyP)
